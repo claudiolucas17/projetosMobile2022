158b15957bb25f977847c491fadb6c025cf04227
claudiolucas17 <claudiolucas17@hotmail.com>
2022-08-12 20:50:25 -0300

    Add files via upload

---
+++ 003-projeto/app.js
@@ -0,0 +1,7 @@
+var salario = 2000.50
+var aumento = 0.15
+var inflacao = 0.07
+novoSalario = salario + (aumento * salario);
+novoSalarioInflacao = novoSalario - (aumento * salario);
+console.log(novoSalario);
+console.log(novoSalarioInflacao);

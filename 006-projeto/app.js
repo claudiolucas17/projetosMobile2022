8c328f3be6660fe47bcc6d8aed899ad5cb2f28c9
claudiolucas17 <claudiolucas17@hotmail.com>
2022-08-16 20:16:56 -0300

    Add files via upload

--- 006-projeto/app.js
+++ 006-projeto/app.js
@@ -14,3 +14,23 @@ console.log(`O ${num2} é maior que ${num1} e ${num1} é maior que ${num3}`);
 console.log(`O ${num2} é menor que ${num1} e ${num1} é menor que ${num3}`);
 
 }
+
+if(num2 > num1 && num1 > num3){
+
+    console.log(`O ${num2} é maior que ${num1} e ${num1} é maior que ${num3}`);
+    
+    }else{
+    
+    console.log(`O ${num1} é menor que ${num2}`);
+    
+    }
+
+    if(num2 > num1 || num1 > num3){
+
+        console.log(` 1 - ${num2} ${num1} ${num1} ${num3}`);
+        
+        }else{
+        
+        console.log(`2 - ${num2} ${num1} ${num1} ${num3}`);
+        
+        }

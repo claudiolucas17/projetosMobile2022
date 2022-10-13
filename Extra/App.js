cec8dfa6bbd7f694aefeaaefa3431d49fae6494c
claudiolucas17 <claudiolucas17@hotmail.com>
2022-10-13 15:41:04 -0300

    Add files via upload

--- Extra/App.js
+++ Extra/App.js
@@ -1,25 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState, Component } from "react";
 import { StyleSheet, Text, View, Button, } from 'react-native';
-import {TextInput} from 'react-native-paper';
 
 export default function App() {
 
   const [acerto, setAcerto ] = useState(0);
   const [erri, setErri ] = useState(0); 
   const [numerro, setNumerro] = useState(0);
- 
+  const [GameStart, setGameStart] = useState(false);
+  const [Sorte, setSorte] = useState (0);
+  const [Pastnumber, setPastnumber] = useState (0);
 
-  const SubirNum = () => {
-    var sorte = Math.random()*100
-    sorte = parseInt(sorte);
-    setNumerro(sorte);
-   };
+  function randomizer(){
+
+    let ssorte = Math.random()*100
+    ssorte = parseInt(ssorte);
+    setNumerro(ssorte);
+    SettingsUAL();
+  }
+
+  function SettingsUAL(){
+
+    
+    setPastnumber(numerro);
+    console.log("Numerro " + numerro);
+    console.log("Pastnumber " + Pastnumber);
+  }
+
+  const Start001 = () => {
+
+    if(GameStart == false){
+
+    console.log("Game Start agora é True!");
+    randomizer();
+    setPastnumber(numerro);
+    setGameStart(true);
+
+    }else{
+
+      alert("Você Já Startou o Game!");
+
+    }
+    
+   }
+
+   const SubirNum = () => {
+
+    if(GameStart == true){
+      console.log("O past number antes da verificação"+Pastnumber);
+      if(Pastnumber >= numerro){
+        
+        
+        setAcerto((acerto+1));
+        randomizer();
+        console.log(Pastnumber);
+        
+      }else{
+
+        
+        setErri((erri+1));
+        randomizer();
+        console.log(Pastnumber);
+      }
+
+    }else{
+
+        alert("Dê start no game primeiro");
+
+    }
+
+   }
   
    const DescerNum = () => {
-    var sorte = Math.random()*100
-    sorte = parseInt(sorte);
-    setNumerro(sorte);
+    console.log("Não Funciona Por Enquanto!");
    }
 
 
@@ -29,42 +82,13 @@ export default function App() {
       <Text>{`Erros:${erri}`}</Text>
       <Text>{`Número:${numerro}`}</Text>
       <Button title="⬆" style={styles.buttonstyle} onPress={() =>{SubirNum();}} color="#6200EE"/>
+      <Button title="Start" style={styles.buttonstyle} onPress={() =>{Start001();}} color="#6200EE"/>
       <Button title="⬇" style={styles.buttonstyle} onPress={() =>{DescerNum();}} color="#6200EE"/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-/*
-export default function App() {
-
-  const [altura, SetAltura] = useState(0);
-  const [base, SetBase] = useState(0); 
-  const [area, SetArea] = useState(0);
- 
-
-  const validateAll = () => {
-
-    SetArea(base*altura/2);
-
-
-   }
-    
-
-
-  return (
-    <View style={styles.container}>
-      <TextInput style={styles.inputText} placeholder="Informe a Base"  onChangeText={base => SetBase(base)}/>{"\n"}
-      <TextInput style={styles.inputText} placeholder="Informe a Altura"  onChangeText={altura => SetAltura(altura)}/>{"\n"}
-      <Button title="Calcular Area" style={styles.buttonstyle} onPress={() =>{validateAll();}} color="#6200EE"/>
-      <Text>{`Base:${base}`}</Text>
-      <Text>{`Altura:${altura}`}</Text>
-      <Text>{`Área:${area}`}</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
